82379f8471ba13be9476c8315daa3bdb792f9ec0
mfahadtahir <muhammadfahad325302@gmail.com>
2018-08-08 18:06:30 +0500

    Update index.js

--- index.js
+++ index.js
@@ -10,8 +10,6 @@ if ('serviceWorker' in navigator) {
 var inputvalue;
 function getData() {
   console.log("testing");
-  var ulfordelete = document.getElementById("ulHtml").childNodes;
-
 
   inputvalue = document.getElementById("inputSection").value;   
   fetch(`https://api.github.com/users/${inputvalue}`).then(function(response){
@@ -25,77 +23,30 @@ function getData() {
     var name = document.getElementById("nameHtml");
     var login = document.getElementById("loginHtml");
     var id = document.getElementById("idHtml");
-    var ul = document.getElementById("ulHtml");
-    var h3 = document.getElementById("headingfollowerHtml");
-    var uldiv = document.getElementById("uldiv");
+    var followers = document.getElementById("followersHtml");
 
+      
     img.setAttribute('src',user.avatar_url);
     img.setAttribute("class", "userimage");
     maindiv.setAttribute("class", "maindiv");
-    uldiv.setAttribute("class", "maindiv");
-    ul.setAttribute("class", "mainul");
-    h3.setAttribute("class", "mainh3")
     name.innerHTML = "Name: ";
     login.innerHTML = "Login: ";
     id.innerHTML = "Id: ";
-    h3.innerHTML = "Followers: "
+    id.innerHTML = "Followers: ";
+      
 
     var loginSite = document.createTextNode(user.login)
     var nameSite = document.createTextNode(user.name);
     var idSite = document.createTextNode(user.id);
-
-
+    var followerSite = document.createTextNode(user.followers);
+      
     name.appendChild(nameSite);
     login.appendChild(loginSite);
     id.appendChild(idSite);
-
-    fetch(`https://api.github.com/users/${inputvalue}/followers`)
-    .then(function(response){
-      return response.json()
-    })
-    .then(function(followers){
-      console.log(followers);
-      var num = 0; 
-      for(khuar in followers){
-        const follSingle = document.createElement("li");
-        num++;
-        follSingle.setAttribute("id",`listnumber${num}`)
-        var followersName = followers[khuar].login;
-        var textforusername = document.createTextNode(followersName);
-        follSingle.appendChild(textforusername);
-        ul.appendChild(follSingle);
-
-      }
-    
-    }).catch(function(error){
-      const noUserFound = document.createTextNode("The user doesnot have any followers");
-      ul.appendChild(noUserFound);
-    })
-  
+    followers.appendChild(followerSite);
     
   })
   .catch(function(error){
     console.log("Not working!!")
   })
 }
-
-
-// caches.match(`https://api.github.com/users/${inputvalue}`)
-// .then(function(response) {
-//   if (!response==null || !response==undefined){
-//     console.log("data found");
-//     return response.json(); 
-//   }else{
-//     console.log("No data");  
-//   }
-// })
-// .then(function(data) {
-//   if(data==null || data==undefined){}
-//   else{
-//   console.log("data from client", data);
-//   //return data;
-//   }
-// })
-// .catch(function() {
-//   console.log("error");
-// })
