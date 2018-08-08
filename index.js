31929727bde8988a3f1f7a1bde068e6c9aa158c8
mfahadtahir <muhammadfahad325302@gmail.com>
2018-08-08 17:57:09 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-console.log("hello world");
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('/sw.js').then(function(registration) {
       // Registration was successful
@@ -8,3 +7,95 @@ if ('serviceWorker' in navigator) {
       console.log('ServiceWorker registration failed: ', err);
     });
 }
+var inputvalue;
+function getData() {
+  console.log("testing");
+  var ulfordelete = document.getElementById("ulHtml").childNodes;
+
+
+  inputvalue = document.getElementById("inputSection").value;   
+  fetch(`https://api.github.com/users/${inputvalue}`).then(function(response){
+    return response.json()
+  })
+  .then(function(user){
+    console.log(user);
+
+    var maindiv = document.getElementById("displayDiv");
+    var img = document.getElementById("imgHtml");
+    var name = document.getElementById("nameHtml");
+    var login = document.getElementById("loginHtml");
+    var id = document.getElementById("idHtml");
+    var ul = document.getElementById("ulHtml");
+    var h3 = document.getElementById("headingfollowerHtml");
+    var uldiv = document.getElementById("uldiv");
+
+    img.setAttribute('src',user.avatar_url);
+    img.setAttribute("class", "userimage");
+    maindiv.setAttribute("class", "maindiv");
+    uldiv.setAttribute("class", "maindiv");
+    ul.setAttribute("class", "mainul");
+    h3.setAttribute("class", "mainh3")
+    name.innerHTML = "Name: ";
+    login.innerHTML = "Login: ";
+    id.innerHTML = "Id: ";
+    h3.innerHTML = "Followers: "
+
+    var loginSite = document.createTextNode(user.login)
+    var nameSite = document.createTextNode(user.name);
+    var idSite = document.createTextNode(user.id);
+
+
+    name.appendChild(nameSite);
+    login.appendChild(loginSite);
+    id.appendChild(idSite);
+
+    fetch(`https://api.github.com/users/${inputvalue}/followers`)
+    .then(function(response){
+      return response.json()
+    })
+    .then(function(followers){
+      console.log(followers);
+      var num = 0; 
+      for(khuar in followers){
+        const follSingle = document.createElement("li");
+        num++;
+        follSingle.setAttribute("id",`listnumber${num}`)
+        var followersName = followers[khuar].login;
+        var textforusername = document.createTextNode(followersName);
+        follSingle.appendChild(textforusername);
+        ul.appendChild(follSingle);
+
+      }
+    
+    }).catch(function(error){
+      const noUserFound = document.createTextNode("The user doesnot have any followers");
+      ul.appendChild(noUserFound);
+    })
+  
+    
+  })
+  .catch(function(error){
+    console.log("Not working!!")
+  })
+}
+
+
+// caches.match(`https://api.github.com/users/${inputvalue}`)
+// .then(function(response) {
+//   if (!response==null || !response==undefined){
+//     console.log("data found");
+//     return response.json(); 
+//   }else{
+//     console.log("No data");  
+//   }
+// })
+// .then(function(data) {
+//   if(data==null || data==undefined){}
+//   else{
+//   console.log("data from client", data);
+//   //return data;
+//   }
+// })
+// .catch(function() {
+//   console.log("error");
+// })
--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-var cacheName = 'my-big-cache';
+var cacheName = 'myCaches';
+var dynamicCache = 'mydynamicCache';
 var filesToCache = [
   '/style.css',
   '/index.js',
@@ -8,37 +9,49 @@ var filesToCache = [
 
 
 
-self.addEventListener('install', function(e) {
+self.addEventListener('install', function (e) {
   console.log('[ServiceWorker] Install');
   e.waitUntil(
     caches.open(cacheName)
-    .then(function(cache) {
-      console.log('[ServiceWorker] Caching app shell');
-      return cache.addAll(filesToCache);
-    })
+      .then(function (cache) {
+        console.log('[ServiceWorker] Caching app shell');
+        return cache.addAll(filesToCache);
+      })
   );
 });
 
-self.addEventListener('activate', function(e) {
-    console.log('[ServiceWorker] Activate');
-    e.waitUntil(
-      caches.keys().then(function(keyList) {
-        return Promise.all(keyList.map(function(key) {
-          if (key !== cacheName) {
-            console.log('[ServiceWorker] Removing old cache', key);
-            return caches.delete(key);
-          }
-        }));
-      })
-    );
-    return self.clients.claim();
-  });
+self.addEventListener('activate', function (e) {
+  console.log('[ServiceWorker] Activate');
+  e.waitUntil(
+    caches.keys().then(function (keyList) {
+      return Promise.all(keyList.map(function (key) {
+        if (key !== cacheName) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  return self.clients.claim();
+});
 
-self.addEventListener('fetch', function(e) {
-  console.log('[ServiceWorker] Fetch', e.request.url);
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-    return response || fetch(e.request);
+
+self.addEventListener('fetch', function (e) {
+  if (e.request.url.startsWith("https://api.github")) {
+    e.respondWith(
+      caches.open('dynamicCache').then(function (cache) {
+        return fetch(e.request).then(function (response) {
+          cache.put(e.request, response.clone());
+          return response;
+        })
       })
     );
-  });
+  }
+else {
+    e.respondWith(
+      caches.match(e.request).then(function (response) {
+        return response || fetch(e.request);
+      })
+    );
+  };
+});
